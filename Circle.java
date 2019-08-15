32eacf2e05deecc878ae1707c235471f94759f80
Jose Bezerra <48967832+bejose@users.noreply.github.com>
2019-08-15 19:02:15 +0300

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Circle
+{
+
+    public static void main (String [] args)
+    {
+
+        //Scanner scan = new Scanner (System.in);
+        //System.out.println ("This program calculates the areas " + "and the perimeters of the excircle and the incircle "
+          //  + "of a given rectangle");
+        //System.out.print("Please enter the two coordinates of the " + "left-upper point of the rectangle");
+        //int leftUpX = scan.nextInt();
+        //int leftUpY = scan.nextInt();
+
+        //System.out.print("Please enter the two coordinates of the" + "right-lower point of the rectangle");
+        //int rightDownX = scan.nextInt();
+        //int rightDownY = scan.nextInt();
+
+        //int areaExcircle = leftUpX * rightDownX;
+        //int perimeterExcircle = 2 * (leftUpX + rightDownX);
+
+        //int areaIncircle = leftUpY * rightDownY;
+        //int perimeterIncircle = 2 * (leftUpX + rightDownY);
+
+        //System.out.println("The area of Excircle is " + areaExcircle);
+        //System.out.println("The perimeter of Excircle is " + perimeterExcircle);
+
+        //System.out.println("The area of Incircle is " + areaIncircle);
+        //System.out.println("The perimeter of Incircle is " + perimeterIncircle);
+
+        
+        
+        
+    
+    }
+}
